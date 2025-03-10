79b7b6afbf1f4e8d21b7ee20e97c59a48ecdd80d
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-10 15:21:30 +0530

    express setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
-
+const cors = require("cors");
 const app=express();
 
 app.listen(4000,()=>{
     console.log("Server is running on port 3000");
 });
+
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    method: ["GET","POST"],
+    credentials: true,
+})
+);
+
+app.use(express.json());
