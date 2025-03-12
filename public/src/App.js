d3ecadfcd2beea1fdcbaa329ec0a07f76140e70b
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-12 16:19:20 +0530

    frontend setup

--- public/src/App.js
+++ public/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route } from "react-router-dom";
+import Secret from "./pages/Secret";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+
+
 
 export default function App() {
   return (
-    <div>
-      App
-    </div>
-  )
+    <BrowserRouter>
+    <Routes>
+     <Route exact path="/register" element={<Register/>}/>
+     <Route exact path="/login" element={<Login/>}/>
+     <Route exact path="/" element={<Secret/>}/>
+    </Routes>
+    
+    
+    </BrowserRouter>
+   
+  );
 }
 
--- server/index.js
+++ server/index.js
@@ -4,21 +4,22 @@ const mongoose = require("mongoose");
 const app=express();
 
 app.listen(4000,()=>{
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 4000");
 });
+
+
+mongoose.connect("mongodb+srv://Rajshekhar25:t7ThRelmPUn4vi8u@cluster0.mnk30.mongodb.net/jwt",{
+    serverSelectionTimeoutMS: 5000
+   
+}).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch(err=>{console.log(err.message)});
 
 //what cors does is it allows us to make requests from our frontend to our backend(which are hosted on different domains)
 //browser by default blocks requests from different domains for security reasons
 
-mongoose.connect("mongodb+srv://Rajshekhar25:t7ThRelmPUn4vi8u@cluster0.mongodb.net/myDatabase",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-});
-
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000"],  //frontend url
     method: ["GET","POST"],
     credentials: true,
 })
