4e8225258f12c3a49f39277fa6e56eac2faa41e9
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-12 16:27:10 +0530

    F-end

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function register() {
   return (
-    <div>
-      Register
-    </div>
-  )
+    <div className="container">
+    <h2> Register Account </h2>
+    <form> 
+      <div>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' placeholder="Email"/>
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input type='password' name='password' id='password' placeholder="Password"/>
+        </div>
+        
+         </form>
+        </div>
+  );
 }
