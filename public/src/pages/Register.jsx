a045e9073802ffe16d2c46fa560fb922c26b8f52
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-13 02:08:48 +0530

    regiistration

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import {ToastContainer, toast} from 'react-toastify';
 
 export default function register() {
+             const[values,setValues]=useState({
+               email:'',
+               password:''
+             });
+  
+   const handleSubmit = (e)=>{    
+    e.preventDefault();
+   };      
   return (
     <div className="container">
     <h2> Register Account </h2>
-    <form> 
+    <form onSubmit={(e)=>handleSubmit(e)}> 
       <div>
         <label htmlFor='email'>Email</label>
-        <input type='email' name='email' id='email' placeholder="Email"/>
+        <input type='email' name='email' id='email' placeholder="Email"  onChnage={(e)=>setValues({ ...values, [e.target.name]: e.target.value })}
+        />
         </div>
-        <div>
+        <div> 
           <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' placeholder="Password"/>
+          <input
+           type='password' name='password' id='password' placeholder="Password"  onChange={(e)=>setValues({ ...values, [e.target.name]: e.target.value })}/>
         </div>
         
         <button type='submit'>Register</button>
