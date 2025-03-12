45bd1ba233dd66fe4e4e066259b3875d3d04ec33
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-12 13:13:43 +0530

    mongo setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app=express();
 
 app.listen(4000,()=>{
     console.log("Server is running on port 3000");
+});
+
+//what cors does is it allows us to make requests from our frontend to our backend(which are hosted on different domains)
+//browser by default blocks requests from different domains for security reasons
+
+mongoose.connect("mongodb+srv://Rajshekhar25:t7ThRelmPUn4vi8u@cluster0.mongodb.net/myDatabase",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
 });
 
 app.use(cors({
