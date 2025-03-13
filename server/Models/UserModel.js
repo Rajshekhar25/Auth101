d65a7534e83578939c2f8ac8bc813da0c49f1d0b
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-13 17:19:32 +0530

    mongoose setup

---
+++ server/Models/UserModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true,"Password is required"],    
+        min: 6
+    }
+});
+
+module.exports = mongoose.model("User",userSchema);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./Routes/AuthRoutes");
+
 const app=express();
+
+const cookieParser = require("cookie-parser");
 
 app.listen(4000,()=>{
     console.log("Server is running on port 4000");
@@ -25,4 +29,8 @@ app.use(cors({
 })
 );
 
+
+app.use(cookieParser());
 app.use(express.json());
+
+app.use("/",authRoutes);
