a72e27583ad42bbc60c6e15a4d6d84470e076adb
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-03-13 17:30:08 +0530

    auth

--- server/Controllers/AuthControllers.js
+++ server/Controllers/AuthControllers.js
@@ -1,2 +1,13 @@
+const UserModel = require('../Models/UserModel');
+
 module.exports.register=async (req, res, next) => {};
-module.exports.login=async (req, res, next) => {};
+
+
+module.exports.login=async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user =await UserModel.create({email,password});
+    } catch (err) {}
+
+
+};
